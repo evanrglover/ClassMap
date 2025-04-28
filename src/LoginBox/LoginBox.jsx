88a70ba6d24f822155addcb25aa06f902eb924bf
Evan Glover <evanrglover@gmail.com>
2025-04-28 12:52:33 -0600

    fixed login path

--- src/LoginBox/LoginBox.jsx
+++ src/LoginBox/LoginBox.jsx
@@ -20,7 +20,7 @@ const Login = ({ setToken }) => {
       //local
       // const response = await axios.post(`http://127.0.0.1:5000/login/${school}`, { email, password });
       //render
-      const response = await axios.post("https://ClassMap.onrender.com/login", { email, password });
+      const response = await axios.post(`https://ClassMap.onrender.com/login${school}`, { email, password });
       
       // Store token
       localStorage.setItem("token", response.data.access_token);
